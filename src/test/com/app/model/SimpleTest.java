c1a820be23f6873ead4e98cfc487b07a5cd17070
Mathias Reker <mathias@reker.dk>
2021-04-20 23:35:31 +0200

    test

--- src/test/com/app/model/SimpleTest.java
+++ src/test/com/app/model/SimpleTest.java
@@ -1,11 +1,9 @@
-package io.github.joaomlneto.travis_ci_tutorial_java;
-
-import java.util.*;
+package test.com.app.model;
 
 import static org.junit.Assert.*;
 import org.junit.*;
 
-public class SimpleCalculatorTest {
+public class SimpleTest {
   @Test
   public void testAdd() {
     assertEquals(1, 1);
